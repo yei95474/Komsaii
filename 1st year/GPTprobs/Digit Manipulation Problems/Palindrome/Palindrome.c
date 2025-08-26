74ce325212c168851976172d4faeacaaed2de767
yei95474 <yei95474@gmail.com>
2025-08-26 16:36:15 +0800

    Bulk update

---
+++ 1st year/GPTprobs/Digit Manipulation Problems/Palindrome/Palindrome.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main () {
+    int x, rev, peel;
+    printf("Enter Number");
+    scanf("%d", &x);
+
+    while (x!=0)
+    {
+        /* code */
+    }
+    
+
+}
