351f8793640016ed25eb9f6240455e56d77e499b
yei95474 <yei95474@gmail.com>
2026-03-20 17:56:05 +0800

    Add initial C++ project configuration and source files

---
+++ 1st year/CODEFORCES/Algolympics Elim/B/B.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main () {
+    int n;
+    cin>>n;
+    int people[n];
+    int k1,k2,k;
+    cin>>k1>>k2>>k;
+
+    for(int i=0; i<n; i++ ) {
+        int c;
+        cin>>c;
+        for (int j=0; j<c; j++) {
+            int temp;
+            cin>>temp;
+            people[c]=temp;
+    }
+}
+
+for (int i=0; i<n; i++){
+    cout<<people[i];
+}
+}
